b3a13131a7bc4c1b3e3d9805f799724476a9310b
Andrei Zubov <andrei.zubov@westwing.de>
2022-03-19 18:52:58 +0000

    initial experiments

---
+++ src/components/MessageBox.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="message" v-bind:class="classObject">{{ message }}</div>
+</template>
+
+<style scoped>
+.message {
+  height: 20px;
+  padding: 5px;
+  margin: 5px;
+}
+
+.success {
+  background: greenyellow;
+}
+
+.warning {
+  background: lightyellow;
+}
+</style>
+
+<script>
+export default {
+  name: "MessageBox",
+  props: {
+    messageType: String,
+    message: String,
+  },
+  computed: {
+    classObject: function () {
+      return {
+        warning: this.messageType === "warn",
+        success: this.messageType === "success",
+      };
+    },
+  },
+};
+</script>
